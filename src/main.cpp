544f0dcd9cff7ec064dff45e9e43b3655dec430d
Joshua Wiens <joshuaw@easymetrics.com>
2020-04-26 23:02:21 -0500

    chore: Adds proj requirements

--- src/main.cpp
+++ src/main.cpp
@@ -4,3 +4,27 @@ int main(int argc, char *argv[])
 {
 	std::cout << "Hello world!" << std::endl;
 }
+
+// Create an empty Tic_Tac_Toe board
+
+// Display the Game Instructions
+
+// Determine who goes first
+
+// Display the board
+
+// While nobody has won and it’s not a tie
+
+// If it’s the human’s turn
+
+// Get the human’s move
+// Update the board with the human’s move
+
+// Calculate the computer’s move
+// Update the board with the computer’s move
+
+// Display the board
+
+// Switch turns
+
+// Congratulate the winner or declare a tie
